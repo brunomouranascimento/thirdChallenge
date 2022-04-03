a0968b3ad4cc7f1f04baafe0bcd337cd6d784cfd
Bruno Nascimento <bruno.bmn@gmail.com>
2022-04-03 14:28:12 -0300

    Initial Commit

---
+++ thirdChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  thirdChallenge
+//
+//  Created by Bruno Nascimento on 03/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ thirdChallenge/thirdChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  thirdChallengeApp.swift
+//  thirdChallenge
+//
+//  Created by Bruno Nascimento on 03/04/22.
+//
+
+import SwiftUI
+
+@main
+struct thirdChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
